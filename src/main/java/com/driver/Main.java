997807b1f0785e2bf5914bcaf5d8f4987387f6e4
Pooja Kale <99339372+Pooja14k@users.noreply.github.com>
2022-10-09 16:02:03 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
+        static class Product{
+                public int product(int x, int y){
+                        return y;
+                }
+                public int product(int x, int y, int z) {
+                        return z;
+                }
+                public double product(double x, double y) {
+                        return x;
+                }
 
+
+        }
+        public static void main(String args[]){
+                Product p = new Product();
+                System.out.println(p.product(12, 34));
+                System.out.println(p.product(1, 3, 5));
+                System.out.println(p.product(1.445, 4.66));
+        }
 }
